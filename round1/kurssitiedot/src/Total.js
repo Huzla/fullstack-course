00bac00445d7fd2213c9993fea45842ebec8c4c8
Lari Alakukku <lari.alakukku@aalto.fi>
2019-06-15 14:05:33 +0300

    Updated Total

--- round1/kurssitiedot/src/Total.js
+++ round1/kurssitiedot/src/Total.js
@@ -1,21 +1,15 @@
 import React from 'react';
 
 class Total extends React.Component {
-  constructor(props) {
-    super(props);
-
-  }
-
-  componentDidMount() {
-  }
-
-  componentWillUnmount() {
-  }
 
   render() {
+    let exercises = [];
 
+    for (let key in this.props.parts) {
+      exercises = exercises.concat(this.props.parts[key].exercises);
+    }
     return (
-      <p>Number of exercises {this.props.exercises.reduce((total, next) => {return total + next}, 0)}</p>
+      <p>Number of exercises {exercises.reduce((total, next) => {return total + next}, 0)}</p>
     );
   }
 }
