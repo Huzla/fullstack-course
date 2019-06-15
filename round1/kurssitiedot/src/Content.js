4e82e98df713606541f1bfe6726051f7f961864a
Lari Alakukku <lari.alakukku@aalto.fi>
2019-06-15 14:05:17 +0300

    Added passing of key to Part to silence a warning

--- round1/kurssitiedot/src/Content.js
+++ round1/kurssitiedot/src/Content.js
@@ -2,12 +2,12 @@ import React from 'react';
 import Part from './Part.js';
 
 class Content extends React.Component {
-  
+
   render() {
     let result = [];
 
     for (let key in this.props.parts) {
-      result = result.concat(<Part name={this.props.parts[key].name} exercises={this.props.parts[key].exercises}/>);
+      result = result.concat(<Part key={key} name={this.props.parts[key].name} exercises={this.props.parts[key].exercises}/>);
     }
     return (result);
   }
