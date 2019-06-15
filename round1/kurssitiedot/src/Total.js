39c650c381679a01f0991a1f85ed1635e96316e4
Lari Alakukku <lari.alakukku@aalto.fi>
2019-06-15 14:15:31 +0300

    Updated Content for 1.4

--- round1/kurssitiedot/src/Total.js
+++ round1/kurssitiedot/src/Total.js
@@ -5,8 +5,8 @@ class Total extends React.Component {
   render() {
     let exercises = [];
 
-    for (let key in this.props.parts) {
-      exercises = exercises.concat(this.props.parts[key].exercises);
+    for (let index in this.props.parts) {
+      exercises = exercises.concat(this.props.parts[index].exercises);
     }
     return (
       <p>Number of exercises {exercises.reduce((total, next) => {return total + next}, 0)}</p>
