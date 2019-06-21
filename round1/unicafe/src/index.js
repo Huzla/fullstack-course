53ba2dd3c966d615d4578865793d95b2e2a806a5
Lari Alakukku <lari.alakukku@aalto.fi>
2019-06-21 14:09:18 +0300

    Added header to unicafe

--- round1/unicafe/src/index.js
+++ round1/unicafe/src/index.js
@@ -1,17 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+const Header = () => (
+  <header>
+  <h1>Feedback is appreciated!</h1>
+  <div class='divider'></div>
+  </header>
+);
+
 const App = () => {
+
   // tallenna napit omaan tilaansa
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
   return (
-    <div>
+    <>
+      <Header />
       code here
-    </div>
+    </>
   )
 }
 
