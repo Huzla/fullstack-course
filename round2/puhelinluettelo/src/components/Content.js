d7d32c63377f2eae938ddb1c692731a3a78ae0ad
Lari Alakukku <lari.alakukku@aalto.fi>
2019-06-22 14:58:20 +0300

    Modified Content to accomodate more complex objects

--- round2/puhelinluettelo/src/components/Content.js
+++ round2/puhelinluettelo/src/components/Content.js
@@ -3,7 +3,7 @@ import Part from './Part.js';
 
 const Content = ({parts}) => {
 
-  let result = parts.map((part) => <Part key={part.name} name={part.name}/>);
+  let result = parts.map((part) => <Part key={part.name} {...part}/>);
 
   return (result);
 }
