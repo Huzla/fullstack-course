0c899c184a2abecf935ac1e4a7c9adaa8b0f62ba
Lari Alakukku <lari.alakukku@aalto.fi>
2019-06-22 14:43:33 +0300

    Modified App for 2.6

--- round2/puhelinluettelo/src/App.js
+++ round2/puhelinluettelo/src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import Content from './components/Content.js';
 
 const App = () => {
   const [ persons, setPersons] = useState([
@@ -6,19 +7,34 @@ const App = () => {
   ])
   const [ newName, setNewName ] = useState('')
 
+  const handleNameChange = (event) => {
+    setNewName(event.target.value);
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    let newPerson = {
+      name: newName
+    };
+
+    setPersons(persons.concat(newPerson));
+    setNewName('');
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={ handleSubmit }>
         <div>
-          name: <input />
+          name: <input value={ newName } onChange={ handleNameChange }/>
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      <Content parts={ persons } />
     </div>
   )
 
