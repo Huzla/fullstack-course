e1c01122735e92ade5902a697c28051555e5a028
Lari Alakukku <lari.alakukku@aalto.fi>
2019-06-22 12:53:43 +0300

    Refactored old class based components to new react

---
+++ round2/kurssitiedot/src/components/Content.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Part from './Part.js';
+
+const Content({parts}) => {
+
+  let result = parts.map((part) => <Part key={part.index} name={part.name} exercises={part.exercises}/>);
+
+  return (result);
+}
+
+export default Content
