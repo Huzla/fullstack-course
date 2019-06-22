3a491ebad2c25fdb46ae47471ee6c7cbc07ef20a
Lari Alakukku <lari.alakukku@aalto.fi>
2019-06-22 12:43:19 +0300

    Init kurssitiedot for 2.1

--- round2/kurssitiedot/src/index.js
+++ round2/kurssitiedot/src/index.js
@@ -1,43 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Header from './Header.js';
-import Content from './Content.js';
-import Total from './Total.js';
+import App from './App.js';
 import './index.css';
-//import * as serviceWorker from './serviceWorker';
 
-const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  };
-
-  return (
-    //Note: ES6 and Babel required!
-    <div>
-      <Header course={course.name}/>
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
-    </div>
-  );
-}
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-//serviceWorker.unregister();
