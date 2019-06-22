d01ed3ada252df46a600e1e9ef64b86f722c2989
Lari Alakukku <lari.alakukku@aalto.fi>
2019-06-22 13:35:31 +0300

    Added functionality for rendering different levels of headings

--- round2/kurssitiedot/src/components/Header.js
+++ round2/kurssitiedot/src/components/Header.js
@@ -1,7 +1,17 @@
 import React from 'react';
 
-const Header = ({course}) => (
-  <h1>{course}</h1>
-)
+const Header = ({course, level}) => {
+
+  //h1,h2,h3,h4,h5 or h6
+  if ( ![...Array.(5).keys().map(i => i+1)].includes(level)  )
+    throw new Error('Invalid level!');
+
+  let const Tag = `h${level}`;
+
+  return (
+    <Tag>{course}</Tag>
+  )
+}
+
 
 export default Header
