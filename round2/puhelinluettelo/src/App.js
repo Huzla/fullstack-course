3ef121b7c785ff3e455113281857786071b69374
Lari Alakukku <lari.alakukku@aalto.fi>
2019-06-22 15:09:04 +0300

    Added support for phonenumbers

--- round2/puhelinluettelo/src/App.js
+++ round2/puhelinluettelo/src/App.js
@@ -3,22 +3,29 @@ import Content from './components/Content.js';
 
 const App = () => {
   const [ persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', num: "040123567" }
   ])
-  const [ newName, setNewName ] = useState('')
+  const [ newName, setNewName ] = useState('');
+  const [ newNum, setNewNum ] = useState('');
 
   const handleNameChange = (event) => {
     setNewName(event.target.value);
+  }
+
+  const handleNumChange = (event) => {
+    setNewNum(event.target.value);
   }
 
   const handleSubmit = (event) => {
     event.preventDefault();
 
     let newPerson = {
-      name: newName
+      name: newName,
+      num: newNum
     };
 
     setNewName('');
+    setNewNum('');
 
     if (persons.find(p => p.name === newPerson.name)) {
       alert(`${newPerson.name} has already been added to the phonebook!`);
@@ -33,7 +40,11 @@ const App = () => {
       <h2>Phonebook</h2>
       <form onSubmit={ handleSubmit }>
         <div>
-          name: <input value={ newName } onChange={ handleNameChange }/>
+          <label htmlFor="name">name: </label>
+          <input name="name" value={ newName } onChange={ handleNameChange }/>
+
+          <label htmlFor="number">number: </label>
+          <input name="number" value={ newNum } onChange={ handleNumChange }/>
         </div>
         <div>
           <button type="submit">add</button>
