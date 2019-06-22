73e9383a7166875538bd3bc39c7134e2237ce20c
Lari Alakukku <lari.alakukku@aalto.fi>
2019-06-22 13:37:41 +0300

    Modified App for 2.4

--- round2/kurssitiedot/src/App.js
+++ round2/kurssitiedot/src/App.js
@@ -1,30 +1,56 @@
 import React from 'react';
-import Course from './components/Course.js'
+import Course from './components/Course.js';
+import Header from './components/Header.js';
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      }
-    ]
-  }
+  const courses = [
+   {
+     name: 'Half Stack application development',
+     parts: [
+       {
+         name: 'Fundamentals of React',
+         exercises: 10,
+         id: 1
+       },
+       {
+         name: 'Using props to pass data',
+         exercises: 7,
+         id: 2
+       },
+       {
+         name: 'State of a component',
+         exercises: 14,
+         id: 3
+       },
+       {
+         name: 'Redux',
+         exercises: 11,
+         id: 4
+       }
+     ]
+   },
+   {
+     name: 'Node.js',
+     parts: [
+       {
+         name: 'Routing',
+         exercises: 3,
+         id: 1
+       },
+       {
+         name: 'Middlewares',
+         exercises: 7,
+         id: 2
+       }
+     ]
+   }
+ ]
+
+ const title = "Web development curriculum"
 
   return (
     <div>
+      <Header level=1 course={ title } />
       <Course course={course} />
     </div>
   )
