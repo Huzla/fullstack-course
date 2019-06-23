e052b7271594b7f96c33d548d6d7c7c55afec25b
Lari Alakukku <lari.alakukku@aalto.fi>
2019-06-23 14:09:07 +0300

    Added conditional rendering to Countries

--- round2/maiden-tiedot/src/components/Countries.js
+++ round2/maiden-tiedot/src/components/Countries.js
@@ -3,11 +3,23 @@ import Country from './Country';
 
 const Countries = ({countries}) => {
 
+  let result = "Too many matches. Please specify.";
 
+  const renderFullCountry = (country) => {
+    return <Country key={ country.name } { ...country }/>;
+  };
+
+  const renderName = (country) => {
+    return <Country key={ country.name } name={ country.name } />;
+  };
+
+  if (countries.length < 10) {
+    result = countries.map( (countries.length === 1) ? renderFullCountry : renderName);
+  }
 
   return (
     <div id="countries">
-      { countries.length }
+      { result }
     </div>
   );
 }
