601099f7839426d94bca9a18276ecb077505cc5a
Lari Alakukku <lari.alakukku@aalto.fi>
2019-06-23 12:49:11 +0300

    Added search field and initial state hooks

--- round2/maiden-tiedot/src/App.js
+++ round2/maiden-tiedot/src/App.js
@@ -1,10 +1,26 @@
-import React from 'react';
-import './App.css';
+import React, {useState, useEffect} from 'react';
+import Search from './components/Search.js';
 
 function App() {
+  const [newSearch, setSearch] = useState('');
+  const [countries, setCountries] = useState([]);
+
+  const suitableCountries = (newSearch === '') ? countries : countries.filter(c => c.name.toUpperCase().includes(newSearch.toUpperCase()));
+//-------------------------------------------------------------------------------------------------------
+
+
+//------------------------------------------------------------------------------------------------------
+
+  const handleSearch = (e) => {
+    setSearch(e.target.value);
+  }
+
+
+
+
   return (
     <div className="App">
-      
+      <Search value={ newSearch } handler={ handleSearch }/>
     </div>
   );
 }
