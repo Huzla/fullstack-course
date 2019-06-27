f6540e7eb705b5bfbb997b5274b0ba6c467394a8
Lari Alakukku <lari.alakukku@aalto.fi>
2019-06-27 20:49:48 +0300

    Added morgan logging

--- round3/puhelinluettelo-backend/server.js
+++ round3/puhelinluettelo-backend/server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const morgan = require('morgan');
 const routes = require('./routes');
 
 const port = 3000;
 
 app.use(bodyParser.json());
+app.use(morgan('tiny'));
 
 app.use('/', routes.general);
 app.use('/api/persons', routes.api);
