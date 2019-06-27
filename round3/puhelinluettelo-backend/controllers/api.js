992d39899957352dc6ef00dab8d4d5352eb94b76
Lari Alakukku <lari.alakukku@aalto.fi>
2019-06-27 19:21:06 +0300

    Finished 3.1

--- round3/puhelinluettelo-backend/controllers/api.js
+++ round3/puhelinluettelo-backend/controllers/api.js
@@ -1,11 +1,10 @@
 const services = require('../services/api.js');
 
 const getPerson = (req, res) => {
-  console.log('request for id:', req.params.id)
   let person = services.findPerson(Number(req.params.id));
 
   if (person)
-    return res.json(person);
+    return res.json( person);
 
   res.status(404).end();
 }
--- round3/puhelinluettelo-backend/server.js
+++ round3/puhelinluettelo-backend/server.js
@@ -1,29 +1,16 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const routes = require('./routes');
+
+const port = 3000;
 
 app.use(bodyParser.json());
 
+app.get('/', (req, res) => {
+  res.send("Hello");
+});
 
-let persons = [
-  {
-    name: "Arto Hellas",
-    number: "040-123456",
-    id: 1
-  },
-  {
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-    id: 2
-  },
-  {
-    name: "Dan Abramov",
-    number: "12-43-234345",
-    id: 3
-  },
-  {
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-    id: 4
-  }
-]
+app.use('/api/persons', routes.api);
+
+app.listen(port, () => console.log(`Server listening on port ${port}`))
