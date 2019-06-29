6e8f19f3631d0d9528b70a5e8e860e92953eb6f4
Lari Alakukku <lari.alakukku@aalto.fi>
2019-06-29 14:35:03 +0300

    Added script for building front end for backend

---
+++ round3/puhelinluettelo-backend/build/precache-manifest.d1819b2566d1e80426182fae8d99fca7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce15a2b5d72585dc635e6598903f5248",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d3f95b94f8b19d93eb26",
+    "url": "/static/css/main.342eab4d.chunk.css"
+  },
+  {
+    "revision": "0f44f52868a50c2f6362",
+    "url": "/static/js/2.d23e1248.chunk.js"
+  },
+  {
+    "revision": "d3f95b94f8b19d93eb26",
+    "url": "/static/js/main.3e29217d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
---
+++ round3/puhelinluettelo-backend/scripts/build.sh
@@ -0,0 +1,4 @@
+rm -rf build
+cd ../puhelinluettelo
+npm run build --prod
+mv build ../puhelinluettelo-backend/
