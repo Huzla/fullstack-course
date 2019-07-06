2ce317c9eadf4b0f87539c170343dc6b6f0f4c9a
Lari Alakukku <lari.alakukku@aalto.fi>
2019-07-06 10:34:11 +0300

    Moved api/blogs routes to their own module

--- round4/blogilista/src/utils/app.js
+++ round4/blogilista/src/utils/app.js
@@ -4,7 +4,7 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const { MONGO_URI } = require("./").config;
-const Blog = require("./../models").blog;
+const routes = require("../routes");
 
 mongoose.connect(MONGO_URI, { useNewUrlParser: true, useCreateIndex: true })
   .then(() => console.log("Connected to database"))
@@ -16,22 +16,6 @@ mongoose.connect(MONGO_URI, { useNewUrlParser: true, useCreateIndex: true })
 app.use(cors());
 app.use(bodyParser.json());
 
-app.get("/api/blogs", (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    });
-});
-
-app.post("/api/blogs", (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    });
-});
+app.use("/api/blogs/", routes.apiBlogs);
 
 module.exports = app;
