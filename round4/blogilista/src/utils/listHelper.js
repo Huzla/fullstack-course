2c20a41c1ee10c5a1be050277b4d362bb66de73e
Lari Alakukku <lari.alakukku@aalto.fi>
2019-07-06 12:07:19 +0300

    Finished 4.5

--- round4/blogilista/src/utils/listHelper.js
+++ round4/blogilista/src/utils/listHelper.js
@@ -5,11 +5,27 @@ const dummy = (blogs) => {
 const totalLikes = (blogs) => {
   if (!blogs.length)
     return 0;
-    
+
   return blogs.map(blog => blog.likes).reduce((acc, cur) => acc + cur);
+};
+
+const favoriteBlog = (blogs) => {
+
+  const max = Math.max(...blogs.map(b => b.likes));
+
+  let result = blogs.find(b => b.likes === max);
+
+  if (result) {
+    delete result._id;
+    delete result.__v;
+    delete result.url;
+  }
+
+  return result;
 };
 
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 };
--- round4/blogilista/tests/listHelper.test.js
+++ round4/blogilista/tests/listHelper.test.js
@@ -60,3 +60,75 @@ describe("total likes", () => {
 
   test("list with many blogs equals the sum of all those", () => expect(listHelper.totalLikes(listWithManyBlogs)).toBe(0+1+123+999));
 });
+
+describe("favorite blog", () => {
+  const listWithOneBlog = [
+    {
+      _id: "5a422aa71b54a676234d17f8",
+      title: "Go To Statement Considered Harmful",
+      author: "Edsger W. Dijkstra",
+      url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+      likes: 5,
+      __v: 0
+    }
+  ];
+
+  const listWithManyBlogs = [
+    {
+        _id: "5d2046850aeaea2a220add16",
+        title: "Testi",
+        author: "Teppo Testaaja",
+        url: "testi.com",
+        likes: 0,
+        __v: 0
+    },
+    {
+        _id: "5d205058b4319330f66e37f2",
+        title: "Testini",
+        author: "Teppo Testaaja",
+        url: "testi.fi",
+        likes: 1,
+        __v: 0
+    },
+    {
+        _id: "5d205058b4319330f66e37f2",
+        title: "Testauksesta",
+        author: "Teppo Testaaja",
+        url: "testi.net",
+        likes: 123,
+        __v: 0
+    },
+    {
+        _id: "5d205058b4319330f66e37f2",
+        title: "Testaajan käsikirja",
+        author: "Teppo Testaaja",
+        url: "testi.ninja",
+        likes: 999,
+        __v: 0
+    }
+  ];
+
+  test("empty list has no favorite and returns falsy", () => {
+    expect(listHelper.favoriteBlog([])).toBeFalsy();
+  });
+
+  test("when list has only one blog that is the favorite", () => {
+    const correct = {
+      title: "Go To Statement Considered Harmful",
+      author: "Edsger W. Dijkstra",
+      likes: 5,
+    };
+
+    expect(listHelper.favoriteBlog(listWithOneBlog)).toEqual(correct);
+  });
+
+  test("in a list with many blogs the favorite has most likes", () => {
+    const correct = {
+      title: "Testaajan käsikirja",
+      author: "Teppo Testaaja",
+      likes: 999
+    };
+
+    expect(listHelper.favoriteBlog(listWithManyBlogs)).toEqual(correct)
+  });
+});
