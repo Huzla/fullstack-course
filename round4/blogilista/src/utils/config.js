c004ce2b367200930bc9b6a437855e6589572a76
Lari Alakukku <lari.alakukku@aalto.fi>
2019-07-13 07:40:41 +0300

    Added test environment

--- round4/blogilista/src/utils/config.js
+++ round4/blogilista/src/utils/config.js
@@ -1,6 +1,10 @@
 require("dotenv").config();
 
-const MONGO_URI = process.env.MONGO_URI || "MONGO_URI=mongodb://localhost/bloglist";
+let MONGO_URI = process.env.MONGO_URI || "MONGO_URI=mongodb://localhost/bloglist";
+
+if (process.env.NODE_ENV === 'test')
+  MONGO_URI = process.env.TEST_MONGO_URI;
+
 const PORT = process.env.PORT || 4000;
 
 module.exports = {
