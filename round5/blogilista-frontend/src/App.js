5e8a4b80c9dec5c31eef91136d0379c8ceac9314
Lari Alakukku <lari.alakukku@aalto.fi>
2019-07-23 15:24:09 +0300

    Added LoginView

--- round5/blogilista-frontend/src/App.js
+++ round5/blogilista-frontend/src/App.js
@@ -1,12 +1,33 @@
-import React from 'react';
-
+import React, { useState, useEffect } from 'react';
+import blogService from './services/blogs.js';
+import LoginForm from './components/LoginView/LoginForm';
 
 function App() {
+  const [blogs, setblogs] = useState([]);
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [user, setUser] = useState(null);
+
+
+  const handleLogin = async (event) => {
+    event.preventDefault();
+    console.log("login");
+  };
+
+  if (user === null) {
+    return (
+      <div className="App">
+        <h2>Please login</h2>
+        <LoginForm username={ username } password={ password } setPassword={ setPassword } setUsername={ setUsername } handleLogin={ handleLogin }/>
+      </div>
+    );
+  }
+
   return (
     <div className="App">
-      
+
     </div>
   );
-}
+};
 
 export default App;
--- round5/blogilista-frontend/src/components/BlogView/BlogList.js
+++ round5/blogilista-frontend/src/components/BlogView/BlogList.js
@@ -3,7 +3,7 @@ import Blog from './Blog.js';
 
 const BlogList = ({ blogs }) => {
 
-  let result = blogs.map((blog) => <Blog key={ blog.id } {...part} />);
+  let result = blogs.map((blog) => <Blog key={ blog.id } blog={ blog } />);
 
   return (result);
 }
