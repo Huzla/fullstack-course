28af32e42b67d950e002fdfba26486197ac9a6c3
Lari Alakukku <lari.alakukku@aalto.fi>
2019-07-27 14:34:24 +0300

    Fixed lint issues in LoginForm

--- round5/blogilista-frontend/src/components/LoginView/LoginForm.js
+++ round5/blogilista-frontend/src/components/LoginView/LoginForm.js
@@ -1,30 +1,39 @@
-import React from 'react';
+import PropTypes from "prop-types";
+import React from "react";
 
 const LoginForm = ({ username, password, setPassword, setUsername, handleLogin }) => (
   <div>
     <h2>Please login</h2>
-  <form onSubmit={ handleLogin }>
-    <div>
-      username
+    <form onSubmit={ handleLogin }>
+      <div>
+        username
         <input
-        type="text"
-        value={ username }
-        name="Username"
-        onChange={({ target }) => setUsername(target.value)}
-      />
-    </div>
-    <div>
-      password
+          type="text"
+          value={ username }
+          name="Username"
+          onChange={({ target }) => setUsername(target.value)}
+        />
+      </div>
+      <div>
+        password
         <input
-        type="password"
-        value={password}
-        name="Password"
-        onChange={({ target }) => setPassword(target.value)}
-      />
-    </div>
-    <button type="submit">login</button>
-  </form>
+          type="password"
+          value={password}
+          name="Password"
+          onChange={({ target }) => setPassword(target.value)}
+        />
+      </div>
+      <button type="submit">login</button>
+    </form>
   </div>
-)
+);
 
-export default LoginForm
+LoginForm.propTypes = {
+  username: PropTypes.string.isRequired,
+  password: PropTypes.string.isRequired,
+  setPassword: PropTypes.func.isRequired,
+  setUsername: PropTypes.func.isRequired,
+  handleLogin: PropTypes.func.isRequired
+};
+
+export default LoginForm;
