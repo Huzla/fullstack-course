0e3de4e45fc6f8088920af1801cf19fd43432684
Lari Alakukku <lari.alakukku@aalto.fi>
2019-07-27 14:27:17 +0300

    Fixed lint issues in BlogForm

--- round5/blogilista-frontend/src/components/BlogView/BlogForm.js
+++ round5/blogilista-frontend/src/components/BlogView/BlogForm.js
@@ -1,39 +1,46 @@
-import React from 'react';
+import PropTypes from "prop-types";
+import React from "react";
 
 const BlogForm = ({ values, setters, handleSubmit }) => (
   <div>
     <h3>Create blog</h3>
-  <form onSubmit={ handleSubmit }>
-    <div>
-      <label htmlFor="title">Title:</label>
-      <input
-        type="text"
-        value={ values.title }
-        name="title"
-        onChange={({ target }) => setters.setTitle(target.value)}
-      />
-    </div>
-    <div>
-      <label htmlFor="author">Author:</label>
-      <input
-        type="text"
-        value={ values.author }
-        name="author"
-        onChange={({ target }) => setters.setAuthor(target.value)}
-      />
-    </div>
-    <div>
-      <label htmlFor="url">Url:</label>
-      <input
-        type="text"
-        value={ values.url }
-        name="url"
-        onChange={({ target }) => setters.setUrl(target.value)}
-      />
-    </div>
-    <button type="submit">Create</button>
-  </form>
+    <form onSubmit={ handleSubmit }>
+      <div>
+        <label htmlFor="title">Title:</label>
+        <input
+          type="text"
+          value={ values.title }
+          name="title"
+          onChange={({ target }) => setters.setTitle(target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="author">Author:</label>
+        <input
+          type="text"
+          value={ values.author }
+          name="author"
+          onChange={({ target }) => setters.setAuthor(target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="url">Url:</label>
+        <input
+          type="text"
+          value={ values.url }
+          name="url"
+          onChange={({ target }) => setters.setUrl(target.value)}
+        />
+      </div>
+      <button type="submit">Create</button>
+    </form>
   </div>
-)
+);
 
-export default BlogForm
+BlogForm.propTypes = {
+  values: PropTypes.object.isRequired,
+  setters: PropTypes.object.isRequired,
+  handleSubmit: PropTypes.func.isRequired
+};
+
+export default BlogForm;
