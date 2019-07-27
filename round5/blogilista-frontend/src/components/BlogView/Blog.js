c7768e341f9639a6571e94736231554e8813c33d
Lari Alakukku <lari.alakukku@aalto.fi>
2019-07-27 12:26:34 +0300

    Finished 5.6

--- round5/blogilista-frontend/src/components/BlogView/Blog.js
+++ round5/blogilista-frontend/src/components/BlogView/Blog.js
@@ -1,8 +1,49 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
+import React,{ useState } from 'react';
+import './css/Blog.css';
+
+const Blog = ({ blog }) => {
+  const [showFull, setShowFull] = useState(false);
+
+  const handleClick = () => {
+    setShowFull(!showFull);
+  };
+
+  const fullBlogElement = () =>(
+
+    <>
+      <div>
+        <strong>{ blog.title }</strong>
+      </div>
+
+      <div>
+        <em>{ blog.author }</em>
+      </div>
+
+      <div>
+        <a href={ blog.url }></a>
+      </div>
+
+      <div>
+        <span>{ blog.likes }</span> likes <button>Like</button>
+      </div>
+
+      <div>
+        added by <em>{ blog.user.name }</em>
+      </div>
+    </>
+);
+
+  const minimizedBlogElement = () => (
+      <>
+      <strong>{ blog.title }</strong> <em>{ blog.author }</em>
+      </>
+  );
+
+  return (
+  <div onClick={ handleClick } className='blog-item'>
+    { (showFull) ? fullBlogElement() : minimizedBlogElement() }
   </div>
-)
+  );
+}
 
 export default Blog
