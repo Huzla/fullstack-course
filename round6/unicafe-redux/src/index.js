d92014cfec190be8ef5a138e3d886aedb2defafe
Lari Alakukku <lari.alakukku@aalto.fi>
2019-08-03 13:56:45 +0300

    Finished 6.2

--- round6/unicafe-redux/src/index.js
+++ round6/unicafe-redux/src/index.js
@@ -1,33 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom'
-import { createStore } from 'redux'
-import reducer from './reducer'
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore } from "redux";
+import reducer from "./reducer";
 
-const store = createStore(reducer)
+const store = createStore(reducer);
 
 const App = () => {
-  const good = () => {
-    store.dispatch({
-      type: 'GOOD'
-    })
-  }
+  const simpleAction = (field) => {
+    return () => {
+      store.dispatch({
+        type: field
+      });
+    };
+  };
+
+  const [ good, ok, bad, zero ] = ["GOOD", "OK", "BAD", "ZERO"].map(key => simpleAction(key));
 
   return (
     <div>
-      <button onClick={good}>hyvä</button> 
-      <button>neutraali</button> 
-      <button>huono</button>
-      <button>nollaa tilastot</button>
-      <div>hyvä {store.getState().good}</div>
-      <div>neutraali</div>
-      <div>huono</div>
+      <button onClick={ good }>hyvä</button>
+      <button onClick={ ok }>neutraali</button>
+      <button onClick={ bad }>huono</button>
+      <button onClick={ zero }>nollaa tilastot</button>
+      <div>hyvä { store.getState().good }</div>
+      <div>neutraali { store.getState().ok }</div>
+      <div>huono { store.getState().bad }</div>
     </div>
-  )
-}
+  );
+};
 
 const renderApp = () => {
-  ReactDOM.render(<App />, document.getElementById('root'))
-}
+  ReactDOM.render(<App />, document.getElementById("root"));
+};
 
-renderApp()
-store.subscribe(renderApp)
+renderApp();
+store.subscribe(renderApp);
