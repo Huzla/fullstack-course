dbcbfcd863a37a4fa33cf5497b15f299d6963a83
Lari Alakukku <lari.alakukku@aalto.fi>
2019-08-03 15:21:27 +0300

    Finished 6.8

--- round6/redux-anecdotes/src/App.js
+++ round6/redux-anecdotes/src/App.js
@@ -2,17 +2,13 @@ import React from "react";
 import AnecdoteList from "./components/AnecdoteList.js";
 import AnecdoteForm from "./components/AnecdoteForm.js";
 
-const App = ({ store }) => {
-  const anecdotes = [...store.getState()];
-
-  return (
-    <div>
-      <h2>Anecdotes</h2>
-      <AnecdoteList { ...{ anecdotes, store } } />
-      <h2>create new</h2>
-      <AnecdoteForm { ...{ store } }/>
-    </div>
-  );
-};
+const App = (props) => (
+  <div>
+    <h2>Anecdotes</h2>
+    <AnecdoteList store={ props.store } />
+    <h2>create new</h2>
+    <AnecdoteForm store={ props.store }/>
+  </div>
+);
 
 export default App;
--- round6/redux-anecdotes/src/components/AnecdoteList.js
+++ round6/redux-anecdotes/src/components/AnecdoteList.js
@@ -2,7 +2,8 @@ import React from "react";
 import Anecdote from "./Anecdote.js";
 import { voteFor } from "../reducers/anecdoteReducer.js";
 
-const AnecdoteList = ({ anecdotes, store }) => {
+const AnecdoteList = ({ store }) => {
+  const anecdotes = [...store.getState()];
 
   return anecdotes.sort((a, b) => a.votes < b.votes).map(anecdote => <Anecdote key={ anecdote.id } { ...anecdote } handleVote={ () => store.dispatch(voteFor(anecdote.id)) } />);
 };
