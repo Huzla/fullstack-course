fb89c0b3d3381a2669c493b32908a7faac48af40
Lari Alakukku <lari.alakukku@aalto.fi>
2019-08-03 13:35:01 +0300

    Added functionality for incrementing GOOD

--- round6/unicafe-redux/src/reducer.js
+++ round6/unicafe-redux/src/reducer.js
@@ -2,22 +2,27 @@ const initialState = {
   good: 0,
   ok: 0,
   bad: 0
-}
+};
+
+const incrementField = (state, field) => {
+  return { ...state, [field]: state[field] + 1 };
+};
 
 const counterReducer = (state = initialState, action) => {
-  console.log(action)
-  switch (action.type) {
-    case 'GOOD':
-      return state
-    case 'OK':
-      return state
-    case 'BAD':
-      return state
-    case 'ZERO':
-      return state
-    default: return state
-  }
-  
-}
+  console.log(action);
 
-export default counterReducer
+  switch (action.type) {
+    case "GOOD":
+      return incrementField(state, "good");
+    case "OK":
+      return state;
+    case "BAD":
+      return state;
+    case "ZERO":
+      return state;
+    default: return state;
+  };
+
+};
+
+export default counterReducer;
