7cd23084a08a9502d28bde0f9f341c8c544683b2
Lari Alakukku <lari.alakukku@aalto.fi>
2019-08-06 18:26:47 +0300

    Finished 6.11

--- round6/redux-anecdotes/src/App.js
+++ round6/redux-anecdotes/src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import AnecdoteList from "./components/AnecdoteList.js";
 import AnecdoteForm from "./components/AnecdoteForm.js";
 import Notification from "./components/Notification.js";
+import Filter from "./components/Filter.js";
 
 const App = (props) => (
   <div>
     <Notification store={ props.store } />
     <h2>Anecdotes</h2>
+    <Filter store={ props.store } />
     <AnecdoteList store={ props.store } />
     <h2>create new</h2>
     <AnecdoteForm store={ props.store }/>
--- round6/redux-anecdotes/src/components/AnecdoteList.js
+++ round6/redux-anecdotes/src/components/AnecdoteList.js
@@ -4,9 +4,11 @@ import { voteFor } from "../reducers/anecdoteReducer.js";
 import { setNotification, clearNotification } from "../reducers/notificationReducer.js";
 
 const AnecdoteList = ({ store }) => {
-  const anecdotes = store.getState().anecdotes;
+  const { anecdotes, filter } = store.getState();
 
-  return anecdotes.sort((a, b) => a.votes < b.votes).map(anecdote => <Anecdote key={ anecdote.id } { ...anecdote } handleVote={ () => {
+  return anecdotes.sort((a, b) => a.votes < b.votes)
+    .filter(anec => anec.content.toUpperCase().includes(filter.toUpperCase()))
+    .map(anecdote => <Anecdote key={ anecdote.id } { ...anecdote } handleVote={ () => {
     store.dispatch(voteFor(anecdote.id));
     store.dispatch(setNotification(`You voted for '${ anecdote.content }'`));
     setTimeout(() => store.dispatch(clearNotification()), 5000);
