8f48d7221c9202487f354c1881d00b5b2e934a4d
Lari Alakukku <lari.alakukku@aalto.fi>
2019-08-09 14:41:28 +0300

    Moved store to its own file and added redux-thunk

--- round6/redux-anecdotes/src/index.js
+++ round6/redux-anecdotes/src/index.js
@@ -1,19 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import { createStore, combineReducers } from "redux";
 import App from "./App.js";
-import anecdoteReducer from "./reducers/anecdoteReducer.js";
-import notificationReducer from "./reducers/notificationReducer.js";
-import filterReducer from "./reducers/filterReducer.js";
-
-const reducer = combineReducers({
-  anecdotes: anecdoteReducer,
-  notification: notificationReducer,
-  filter: filterReducer
-});
-
-const store = createStore(reducer);
+import store from "./store.js";
 
 const render = () => {
   ReactDOM.render(
