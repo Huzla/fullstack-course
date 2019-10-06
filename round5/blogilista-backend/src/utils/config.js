8aa7802deff7d30798b0ab7f0574f21a24e60664
lari alakukku <lari.alakukku@aalto.fi>
2019-10-06 17:43:33 +0300

    added basic firebase support

--- round5/blogilista-backend/src/utils/config.js
+++ round5/blogilista-backend/src/utils/config.js
@@ -3,9 +3,9 @@ require("dotenv").config();
 let MONGO_URI = process.env.MONGO_URI || "MONGO_URI=mongodb://localhost/bloglist";
 
 if (process.env.NODE_ENV === 'test')
-  MONGO_URI = process.env.TEST_MONGO_URI;
+  MONGO_URI = process.env.TEST_MONGO_URI || "MONGO_URI=mongodb://localhost/bloglistTest";
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 3003;
 
 const PASS_LENGTH = (!process.env.PASS_LENGTH || process.env.PASS_LENGTH < 3) ? 3 : process.env.PASS_LENGTH;
 const USERID_LENGTH = (!process.env.USERID_LENGTH || process.env.PASS_LENGTH < 3) ? 3 : process.env.USERID_LENGTH;
